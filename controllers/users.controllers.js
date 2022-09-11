54a6da876ca46caec051fd244d0ac1eeb87b17ec
mnaofficial <mna.official92@gmail.com>
2022-09-11 23:39:07 +0600

    user get, post,patch and delete part setup

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -2,12 +2,20 @@ const fs = require("fs");
 
 // users data file read
 function getUsers() {
-  const data = fs.readFileSync(__dirname + "/../users.json");
-  const users = JSON.parse(data);
-  return users;
+    const data = fs.readFileSync(__dirname + "/../users.json");
+    const users = JSON.parse(data);
+    return users;
 }
 
-// get all users
+// GET: random user
+module.exports.getRandomUser = (req, res) => {
+    const users = getUsers();
+    const randomUser = users[Math.floor(Math.random() * users.length)];
+    res.send(randomUser);
+};
+
+
+// GET: all users
 module.exports.getAllUsers = (req, res, next) => {
     const { limit } = req.query;
     if (limit) {
@@ -17,9 +25,22 @@ module.exports.getAllUsers = (req, res, next) => {
     }
 }
 
-// get random user
-module.exports.getRandomUser = (req, res) => {
-    const users = getUsers();
-    const randomUser = users[Math.floor(Math.random() * users.length)];
-    res.send(randomUser);
-  };
+// POST: save a user
+module.exports.saveAUser = (req, res) => {
+
+}
+
+// PATCH: update a user
+module.exports.updateAUser = (req, res) => {
+
+}
+
+// PATCH: bulk update
+module.exports.bulkUpdate = (req, res) => {
+
+}
+
+// DELETE: delete a user
+module.exports.deleteAUser = (req, res) => {
+
+}
